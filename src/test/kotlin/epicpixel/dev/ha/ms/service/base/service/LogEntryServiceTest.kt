0b999ccaee1b6655f3472077454d88242604d277
Márcia Helena <marciahelenalimac@gmail.com>
2024-04-10 19:07:36 -0300

    teste da regra de negócio
    Este commit inclui a classe de teste LogEntryServiceTest que contém três testes unitários:
    
    test create log with future date: Verifica se o método createLog lança uma exceção quando tenta criar um log com uma data futura.
    test create log with blank details: Verifica se o método createLog lança uma exceção quando tenta criar um log com detalhes em branco.
    test create log with non-existing user: Verifica se o método createLog lança uma exceção quando tenta criar um log com um usuário que não existe no repositório.
    Esses testes ajudam a garantir que o serviço LogEntryService esteja funcionando corretamente e tratando erros conforme esperado. As dependências são simuladas usando a biblioteca Mockito para isolar o serviço que está sendo testado

--- src/test/kotlin/epicpixel/dev/ha/ms/service/base/service/LogEntryServiceTest.kt
+++ src/test/kotlin/epicpixel/dev/ha/ms/service/base/service/LogEntryServiceTest.kt
@@ -1,78 +1,91 @@
-//package epicpixel.dev.ha.ms.service.base.service
-//
-//import epicpixel.dev.ha.ms.service.base.document.LogEntry
-//import epicpixel.dev.ha.ms.service.base.document.LogLevel
-//import epicpixel.dev.ha.ms.service.base.repository.LogEntryRepository
-//import org.junit.jupiter.api.Assertions.*
-//import org.junit.jupiter.api.BeforeEach
-//import org.junit.jupiter.api.Test
-//import org.mockito.ArgumentCaptor
-//import org.mockito.Captor
-//import org.mockito.Mock
-//import org.mockito.Mockito.*
-//import org.mockito.MockitoAnnotations
-//import java.time.Instant
-//
-//class LogEntryServiceTest {
-//
-//    @Mock private lateinit var logEntryRepository: LogEntryRepository
-//    private lateinit var logEntryService: LogEntryService
-//    @Captor private lateinit var logEntryCaptor: ArgumentCaptor<LogEntry>
-//
-//    @BeforeEach
-//    fun setUp() {
-//        MockitoAnnotations.openMocks(this)
-//        logEntryService = LogEntryService(logEntryRepository)
-//    }
-//
-//    @Test
-//    fun `test alterarLog`() {
-//        val logId = "logId"
-//        val logEntry = LogEntry(details = "Updated details")
-//        `when`(logEntryRepository.findById(logId)).thenReturn(java.util.Optional.of(logEntry))
-//        logEntryService.alterarLog(logEntry, logId)
-//
-//        assertEquals("Updated details", logEntry.details)
-//        verify(logEntryRepository, times(1)).findById(logId)
-//        verify(logEntryRepository, times(1)).save(logEntryCaptor.capture())
-//        assertEquals(logEntry, logEntryCaptor.value)
-//    }
-//
-//    @Test
-//    fun `test criarLog`() {
-//        val logEntry = LogEntry(details = "New log details")
-//        `when`(logEntryRepository.count()).thenReturn(99L)
-//        `when`(logEntryRepository.findByDetails(logEntry.details)).thenReturn(null)
-//        logEntryService.criarLog(logEntry)
-//        verify(logEntryRepository, times(1)).save(logEntryCaptor.capture())
-//        assertEquals(logEntry, logEntryCaptor.value)
-//
-//    }
-//
-//    @Test
-//    fun `test excluirLog`() {
-//        val logId = "logId"
-//        `when`(logEntryRepository.findById(logId)).thenReturn(java.util.Optional.of(createDummyLogEntry()))
-//        logEntryService.excluirLog(logId)
-//        verify(logEntryRepository, times(1)).deleteById(logId)
-//    }
-//
-//    @Test
-//    fun `test validarLog`() {
-//        val logEntry = LogEntry(user = null, createdAt = Instant.now(), level = null, details = "")
-//        val exception = assertThrows(IllegalArgumentException::class.java) {
-//            logEntryService.validarLog(logEntry)
-//        }
-//        assertTrue(exception.message?.contains("Campos obrigatórios não preenchidos: user, createdAt, level, details") ?: false)
-//    }
-//
-//    private fun createDummyLogEntry(): LogEntry {
-//        return LogEntry(
-//            user = null,
-//            createdAt = Instant.now(),
-//            level = LogLevel.INFO,
-//            details = "Dummy log details"
-//        )
-//    }
-//
-//}
+package epicpixel.dev.ha.ms.service.base.service
+
+import epicpixel.dev.ha.ms.service.base.document.LogEntry
+import epicpixel.dev.ha.ms.service.base.document.LogLevel
+import epicpixel.dev.ha.ms.service.base.document.UserDocument
+import epicpixel.dev.ha.ms.service.base.repository.LogEntryRepository
+import epicpixel.dev.ha.ms.service.base.repository.UserRepository
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.Instant
+
+@SpringBootTest
+@ExtendWith(MockitoExtension::class)
+class LogEntryServiceTest {
+
+    @Mock
+    private lateinit var logEntryRepository: LogEntryRepository
+
+    @Mock
+    private lateinit var userRepository: UserRepository
+
+    @InjectMocks
+    private lateinit var logEntryService: LogEntryService
+
+
+    //Este teste está verificando se o método createLog da classe LogEntryService lança uma exceção
+    //quando tentamos criar um log com uma data futura
+    @Test
+    fun `test create log with future date`() {
+        // Cria uma data que está 1 hora no futuro
+        val futureDate = Instant.now().plusSeconds(3600)
+
+        // Cria uma nova instância de LogEntry com a data futura
+        val logEntry = LogEntry(details = "Test log", createdAt = futureDate)
+
+        // Espera que uma exceção seja lançada quando tentamos criar um log com uma data futura
+        val exception = assertThrows(IllegalArgumentException::class.java) {
+            // Tenta criar um log com a data futura
+            logEntryService.createLog(logEntry)
+        }
+
+        // Verifica se a mensagem da exceção é a esperada
+        assertEquals("A data fornecida está no futuro.", exception.message)
+    }
+
+    //Este teste está verificando se o método createLog da classe LogEntryService lança uma exceção
+    // quando tentamos criar um log com detalhes em branco
+    @Test
+    fun `test create log with blank details`() {
+        // Cria uma nova instância em branco
+        val logEntry = LogEntry(details = "", level = LogLevel.INFO)
+
+        // Espera que uma exceção seja lançada quando tentamos criar um log com detalhes em branco
+        val exception = assertThrows(IllegalArgumentException::class.java) {
+            logEntryService.createLog(logEntry)
+        }
+        // Verifica se a mensagem da exceção é a esperada
+        assertEquals("Detalhes em branco não são permitidos.", exception.message)
+    }
+
+    /*Esse teste foi projetado para falhar. Ele está testando um cenário em que você tenta criar
+    um log com um usuário que não existe no repositório. O método createLog na classe LogEntryService
+    deve lançar uma exceção
+    */
+    @Test
+    fun `test create log with non-existing user`() {
+        // Cria uma nova instância de UserDocument para um usuário que não existe
+        val nonExistingUser = UserDocument(
+            email = "nonexisting@example.com", password = "password",
+            name = "Non Existing", roles = emptyList()
+        )
+        // Cria uma nova instância de LogEntry com o usuário não existente
+        val logEntry = LogEntry(user = nonExistingUser, details = "Test log")
+
+        // Configura o mock do userRepository para retornar false quando o método existsByEmailIgnoreCase for chamado
+        `when`(userRepository.existsByEmailIgnoreCase(anyString())).thenReturn(false)
+
+        // Espera que uma exceção seja lançada quando tentamos criar um log com um usuário não existente
+        val exception = assertThrows(IllegalArgumentException::class.java) {
+            logEntryService.createLog(logEntry)
+        }
+        // Verifica se a mensagem da exceção é a esperada
+        assertEquals("Usuário não encontrado.", exception.message)
+    }
+}
